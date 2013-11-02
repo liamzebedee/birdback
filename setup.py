6b4576cbc54fe73aea6cbfd9e91012af08999c1a
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-11-02 11:45:10 +1000

    Finally dependency management works with scandir

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(name = "birdback",
 	
 	package_dir = {'birdback' : 'src/birdback'},
 	packages = ['birdback'],
-	dependency_links = ['https://github.com/liamzebedee/scandir/tarball/master#egg=scandir-0.1'],
-	install_requires = [],
+	dependency_links = ['git+https://github.com/liamzebedee/scandir.git#egg=scandir-0.1'],
+	install_requires = ['scandir'],
 	
 	data_files=[('share/icons/hicolor/scalable/apps', ['birdback.svg'])],
 	scripts = [],
