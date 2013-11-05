0f99e82334ad7be40af2fa9d69e7cb91277a7d65
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-11-05 16:14:46 +1000

    Remove dusty test code

--- src/birdback/controller.py
+++ src/birdback/controller.py
@@ -1,28 +1,9 @@
-# While writing this I was listening to Flight of the Conchord's song 'Hiphopopotamus vs. Rhymenoceros'
-# When naming, I thought I'd take a cue from the Rhymenocerous, of whose back the birds lie.
 import scandir
 import time
 	
 class Controller(object):
 	def __init__(self):
 		pass
+	
 	def stop(self):
 		pass
-	def test():
-		count = 0
-		mainBackupDir = '/home/liamzebedee'
-		ignoredDirs = ['.cache', '.ccache', 'Downloads', 'tmp']
-		start = time.time()
-		for root, dirs, files in scandir.walk(mainBackupDir):
-			for ignoredDir in ignoredDirs:
-				if ignoredDir in dirs: dirs.remove(ignoredDir)
-				if root == (mainBackupDir + '/.local/share'):
-					if 'Trash' in dirs: dirs.remove('Trash')
-			for name in files:
-				count += 1
-			print(root)
-		end = time.time()
-		print(str(start))
-		print(str(end))
-		interval = end - start
-		print('Process took ', str(interval))
