37e3ab329d8a62e13360d5cef089740f4a49176b
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-11-09 17:19:27 +1000

    Removed signal code because it doesn't work

--- src/birdback/birdback.py
+++ src/birdback/birdback.py
@@ -4,19 +4,16 @@
 import controller
 import signal
 
-if __name__ == '__main__':
+def main():
 	app = None
 	try:
 		app = controller.Controller()
 		print("Controller instantiated")
-		def signalExit(signal, frame):
-			app.quit()
-		signal.signal(signal.SIGTERM, signalExit)
-		signal.signal(signal.SIGINT, signalExit)
 		app.run()
-	except KeyboardInterrupt:
-		app.quit()
 	except Exception as e:
 		print("Exiting due to critical error:")
 		print(e)
 		app.quit(1)
+
+if __name__ == '__main__':
+	main()
